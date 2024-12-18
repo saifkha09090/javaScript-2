dff6ca68a2abc3881a0d3be3b24e9d9c6d83ddce
Saifullah Khan <saifullahkha09090@gmail.com>
2024-12-18 20:48:10 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,281 @@
+// Chapter no 4
+
+// Question no 1
+
+// let fName = "saif";
+// let lName = "saif";
+// let myName = "saif";
+
+// console.log(fName, lName, myName);
+
+// Question no 2
+
+// let fName;
+// let userName1;
+// let _userName2;
+// let $userNam3;
+// let userNam4;
+
+
+// let 1num;
+// let @num;
+// let #num;
+// let *num;
+// let !num;
+
+// Question no 3
+
+// document.write(`<h1>Rules for naming JS variables</h1> \n <p>Variable names can only contain <strong>letters</strong>, <strong>digits</strong>, <strong>underscores (_)</strong>, and <strong>dollar signs ($)</strong>. For example: <code>$my_1stVariable</code>.</p> \n
+//     <p>Variables must begin with a <strong>letter</strong>, <strong>underscore (_)</strong>, or <strong>dollar sign ($)</strong>. For example: <code>$name</code>, <code>_name</code>, or <code>name</code>.</p> \n
+//     <p>Variable names are case <strong>sensitive</strong>.</p>
+//     <p>Variable names should not be JS <strong>reserved keywords</strong>.</p>`)
+
+
+// Chapter  4 Complete
+
+// Chapter  5   
+
+// Question no 1
+
+// let num = 5;
+// let num2 = 7;
+
+// let sum = num + num2;
+
+// document.write(`<h1>Sum of ${num} and ${num2} is ${sum} </h1>`);
+
+// Question no 2    
+
+
+// let num = 5;
+// let num2 = 7;
+
+// let sub = num - num2;
+
+// document.write(`<h1>sub of ${num} and ${num2} is ${sub} </h1>`);
+
+// let multiply = num * num2;
+
+// document.write(`<h1>multiply of ${num} and ${num2} is ${multiply} </h1>`);
+
+// let divide = num / num2;
+
+// document.write(`<h1>divide of ${num} and ${num2} is ${divide} </h1>`);
+
+// let modulus = num % num2;
+
+// document.write(`<h1>modulus of ${num} and ${num2} is ${modulus} </h1>`);
+
+// Question no 3
+
+
+// let num;
+
+// document.write("<h1>Value after variable declaration is: " + num + "\n </h1>");
+
+// num = 11;
+
+// document.write("<h1>Initial value: " + num + "\n </h1>");
+
+// num++;
+
+// document.write("<h1>Value after increment is: " + num + "\n </h1>");
+
+// num += 6;
+
+// document.write("<h1>Value after addition is: " + num + "\n </h1>");
+
+// num--;
+
+// document.write("<h1>Value after decrement is: " + num + "\n </h1>");
+
+// let remainder = num % 4;
+
+// document.write("<h1>The remainder is: " + remainder + "\n </h1>");
+
+
+// Question no 4
+
+// let ticketPrice = 600;
+
+// let totalCost = ticketPrice * 5;
+
+// document.write("<h1>The cost of buying 5 tickets to a movie is: " + totalCost + " PKR </h1>");
+
+// Question no 5
+
+// let tablenum = 4;
+
+// document.write("<h1> Table of " + tablenum + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 1 = " + tablenum * 1 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 2 = " + tablenum * 2 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 3 = " + tablenum * 3 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 4 = " + tablenum * 4 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 5 = " + tablenum * 5 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 6 = " + tablenum * 6 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 7 = " + tablenum * 7 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 8 = " + tablenum * 8 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 9 = " + tablenum * 9 + "</h1>");
+
+// document.write("<h1>" + tablenum + " X 10 = " + tablenum * 10 + "</h1>");
+
+// Question no 6
+
+// let celsius = 30;
+// let fahrenheitFromCelsius = (celsius * 9/5) + 32;
+
+// document.write(`<h1> ${celsius}°C is ${fahrenheitFromCelsius}°F <br/> </h1>`);
+
+// let fahrenheit = 86;
+// let celsiusFromFahrenheit = (fahrenheit - 32) * 5/9;
+
+// document.write(`<h1> ${fahrenheit}°F is ${celsiusFromFahrenheit}°C </h1>`);
+
+// Question no 7
+
+
+// let priceOfItem1 = 110;     
+// let priceOfItem2 = 90;      
+// let quantityOfItem1 = 4;   
+// let quantityOfItem2 = 8;
+// let shippingCharges = 1000;  
+
+// let totalCost = (priceOfItem1 * quantityOfItem1) + (priceOfItem2 * quantityOfItem2) + shippingCharges;
+
+// document.write("<h1> Price of item 1 is " + priceOfItem1 + "</h1>");
+// document.write("<h1> Quantity of item 1 is " + quantityOfItem1 + "</h1>");
+
+// document.write("<h1> Price of item 2 is " + priceOfItem2 + "</h1>");
+// document.write("<h1> Quantity of item 2 is " + quantityOfItem2 + "</h1>");
+
+// document.write("<h1> Shipping Charges " + shippingCharges + "</h1>");
+
+// document.write(`<h1>Total cost of your order is ${totalCost} </h1>`);
+
+// Question no 8
+
+// let totalMark = 980;
+// let markObtain = 804;
+
+// let percentage = (markObtain / totalMark) * 100;
+
+// document.write("<h1>Total Mark is " + totalMark + "</h1>");
+
+// document.write("<h1>Mark Obtain is " + markObtain + "</h1>");
+
+// document.write(`<h1>Your percentage is ${percentage}% </h1>`);
+
+// Question no 9
+
+// let exchangeRateUSDToPKR = 104.80;
+// let exchangeRateSARToPKR = 28;
+
+// let usd = 10;
+// let sar = 25;
+
+// let totalPKR = (usd * exchangeRateUSDToPKR) + (sar * exchangeRateSARToPKR);
+
+// document.write("<h1>Currency in PKR  </h1> <br/> <br/> <br/> <br/> <br/> <br/>"); 
+// document.write("<h1>Total Currency in " + totalPKR + " PKR</h1> ");
+
+// Question no 10
+
+// let num = 5;
+// let result = (num + 5) * 10 / 2;
+
+// document.write(`<h1>Result of the expression is ${result} </h1>`);
+
+// Question no  11
+
+// let currentYear = 2024;
+// let birthYear = 2004;
+
+// let age = currentYear - birthYear;
+
+// document.write("<h1>Current year is " + currentYear + "</h1>");
+
+// document.write("<h1>Birth year is " + birthYear + "</h1>");
+
+// document.write("<h1>Your age is " + age + "</h1>");
+
+// Question no  12
+
+// let radius = 20;
+// let pi = 3.142;
+
+// let circumference = 2 * pi * radius;
+
+// let area = pi * radius * radius;
+
+// document.write("<h1>The Geometrizer</h1>  <br/> <br/> <br/> <br/> <br/> <br/>");
+
+// document.write("<h1>Radius of the circle is " + radius + "</h1>");
+
+// document.write("<h1>Circumference of the circle is " + circumference + "</h1>");
+
+// document.write("<h1>Area of the circle is " + area + "</h1>");
+
+// Question no 13
+
+// let snack = "Lays";
+// let age = 20;
+// let maximumAge = 60;
+// let amountOfSnack = 4;
+// let totalYearLeft = maximumAge - age;
+// let totalSnackLeft = totalYearLeft * amountOfSnack;
+
+// document.write("<h1>Snack: " + snack + "</h1>");
+
+// document.write("<h1>Age: " + age + "</h1>");
+
+// document.write("<h1>Maximum Age: " + maximumAge + "</h1>");
+
+// document.write("<h1>Amount of Snack: " + amountOfSnack + "</h1>");
+
+// document.write("<h1>You will need " + totalSnackLeft +  " to last you until the ripe old age of " + maximumAge + "</h1>");
+
+// chapter no 5 completed
+
+// Chapter  no 6
+
+// Question no 1
+
+// let num = 10;
+
+// document.write("<h1>Result: <br /> The value of num is: " + num + "</h1>");
+
+// document.write("<h1>...............................................</h1> <br />");
+
+// document.write("<h1>The value of ++num is: " + ++num + "</h1>");
+// document.write("<h1>Now the value of num is: " + num + "</h1> <br />");
+
+// document.write("<h1>The value of num++ is: " + num++ + "</h1>");
+// document.write("<h1>Now the value of num is: " + num + "</h1> <br />");
+
+// document.write("<h1>The value of --num is: " + --num + "</h1>");
+// document.write("<h1>Now the value of num is: " + num + "</h1> <br />");
+
+// document.write("<h1>The value of num-- is: " + num-- + "</h1>");
+// document.write("<h1>Now the value of num is: " + num + "</h1>");
+
+// Question no 2
+
+// let a = 2;
+// let b = 1;
+// let result = --a - --b + ++b + b--;
+
+// document.write("<h1>The value of a is: " + a + "</h1>");
+
+// document.write("<h1>The value of b is: " + b + "</h1>");
+
+// document.write("<h1>Result: " + result + "</h1>");
+
