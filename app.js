725ed168acf462870e11527c67d15061733208ed
Saifullah Khan <saifullahkha09090@gmail.com>
2024-12-22 16:48:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -245,7 +245,7 @@
 
 // chapter no 5 completed
 
-// Chapter  no 6
+// Chapter  no 6 to 9
 
 // Question no 1
 
@@ -279,3 +279,359 @@
 
 // document.write("<h1>Result: " + result + "</h1>");
 
+// Question no 3
+
+// let fullName = prompt("Please enter your full name");
+
+// document.write(`<h1>Hello ${fullName}</h1>`);
+
+// Question no 4
+
+// let tablenum = +prompt("Please enter a table number", 5);
+
+//     document.write("<h1>" + tablenum + " X 1 = " + tablenum * 1 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 2 = " + tablenum * 2 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 3 = " + tablenum * 3 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 4 = " + tablenum * 4 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 5 = " + tablenum * 5 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 6 = " + tablenum * 6 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 7 = " + tablenum * 7 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 8 = " + tablenum * 8 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 9 = " + tablenum * 9 + "</h1>");
+//     document.write("<h1>" + tablenum + " X 10 = " + tablenum * 10 + "</h1>");
+
+// Question no 5
+
+
+// let subject1 = prompt("Enter the name of the first subject");
+// let subject2 = prompt("Enter the name of the second subject");
+// let subject3 = prompt("Enter the name of the third subject");
+
+// let totalMarksPerSubject = 100;
+
+// let obtainedMarksSubject1 = Number(prompt(`Enter the obtained marks for ${subject1}`));
+// let obtainedMarksSubject2 = Number(prompt(`Enter the obtained marks for ${subject2}`));
+// let obtainedMarksSubject3 = Number(prompt(`Enter the obtained marks for ${subject3}`));
+
+// let totalObtainedMarks = obtainedMarksSubject1 + obtainedMarksSubject2 + obtainedMarksSubject3;
+// let totalMarks = totalMarksPerSubject * 3;
+// let percentage = (totalObtainedMarks / totalMarks) * 100;
+
+// document.write(`
+// <table border>
+//     <tr>
+//         <th>Subject</th>
+//         <th>Total Marks</th>
+//         <th>Obtained Marks</th>
+//         <th>Percentage</th>
+//     </tr>
+//     <tr>
+//         <td>${subject1}</td>
+//         <td>${totalMarksPerSubject}</td>
+//         <td>${obtainedMarksSubject1}</td>
+//         <td>${(obtainedMarksSubject1 / totalMarksPerSubject) * 100}%</td>
+//     </tr>
+//     <tr>
+//         <td>${subject2}</td>
+//         <td>${totalMarksPerSubject}</td>
+//         <td>${obtainedMarksSubject2}</td>
+//         <td>${(obtainedMarksSubject2 / totalMarksPerSubject) * 100}%</td>
+//     </tr>
+//     <tr>
+//         <td>${subject3}</td>
+//         <td>${totalMarksPerSubject}</td>
+//         <td>${obtainedMarksSubject3}</td>
+//         <td>${(obtainedMarksSubject3 / totalMarksPerSubject) * 100}%</td>
+//     </tr>
+//     <tr>
+//         <td></td>
+//         <th>${totalMarks}</th>
+//         <th>${totalObtainedMarks}</th>
+//         <th>${percentage}%</th>
+//     </tr>
+// </table>
+// `);
+
+// Chapter no 6 to 9 completed
+
+// Chapter no 9 to 11
+
+// Question no 1
+
+// let city = prompt('enter your city name');
+// if (city === "karachi") {
+//     document.write('<h1>Welcome to city of light</h1>');
+// }
+
+// Question no 2
+
+// let gender = prompt('Enter your gender');
+// if (gender === "male") {
+//     document.write('<h1>Good Morning Sir</h1>');
+// }
+// if (gender === "female") {
+//     document.write('<h1>Good Morning Maam</h1>');
+// }
+
+// Question no 3
+
+// let colorTrafficSignal = prompt('Enter the color of the road traffic signal');
+
+// if (colorTrafficSignal === "red") { 
+//     document.write('<h1>Must Stop </h1>');
+// }
+
+// if (colorTrafficSignal === "yellow") {
+//     document.write('<h1>Ready to move</h1>');
+// }
+
+// if (colorTrafficSignal === "green") {
+//     document.write('<h1>Move now</h1>');
+// }
+
+// Question no 4
+
+// let carFuel = +pompt("Enter your car fuel number Enter remaining fuel in car (in litres):");
+
+// if (carFuel <= 0.25) {
+//     document.write('<h1>Please refill the car fuel</h1>');
+// }
+
+// Question no 5
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// } // alert show
+
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// } //  alert not show
+
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// } //  alert not show
+// if (c === 13){
+// alert("condition 2 is true");
+// } // alert show
+// if (++c < 14){
+// alert("condition 3 is true");
+// } //  alert not show
+// if(c === 14){
+// alert("condition 4 is true");
+// } // alert show
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// } // alert show
+
+
+// if (true){
+// alert("True");
+// } // alert show
+// if (false){
+// alert("False");
+// } //  alert not show
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// } // alert show
+
+// Question no 6
+
+
+// let obtainedMarksSubject1 = Number(prompt("Enter the obtained marks for Subject 1:"));
+// let obtainedMarksSubject2 = Number(prompt("Enter the obtained marks for Subject 2:"));
+// let obtainedMarksSubject3 = Number(prompt("Enter the obtained marks for Subject 3:"));
+// let totalMarks = 300;
+
+// let ObtainedMarks = obtainedMarksSubject1 + obtainedMarksSubject2 + obtainedMarksSubject3;
+
+// let percentage = Math.floor((ObtainedMarks / totalMarks) * 100);
+
+// document.write("<h1>Marks Sheet</h1>")
+
+// if (percentage >= 80) {
+//     document.write(`<h1>Total marks : ${totalMarks} <br/> Marks obtained : ${ObtainedMarks} <br /> Percentage : ${percentage}% <br /> Grade : A-one <br /> Remarks : Excellent </h1>`);
+// } else if(percentage >= 70) {
+//     document.write(`<h1>Total marks : ${totalMarks} <br/> Marks obtained : ${ObtainedMarks} <br /> Percentage : ${percentage}% <br /> Grade : A <br /> Remarks : Good </h1>`);
+// } else if (percentage >= 60) {
+//     document.write(`<h1>Total marks : ${totalMarks} <br/> Marks obtained : ${ObtainedMarks} <br /> Percentage : ${percentage}% <br /> Grade : B <br /> Remarks : You need to improve </h1>`);
+// } else if (percentage <= 59){ 
+//     document.write(`<h1>Total marks : ${totalMarks} <br/> Marks obtained : ${ObtainedMarks} <br /> Percentage : ${percentage}% <br /> Grade : Fail <br /> Remarks : Sorry </h1>`);}
+
+// Question no 7
+
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userGuess = Number(prompt("Enter your secret number 1 to 10"));
+
+// if (userGuess === secretNumber) {
+//     alert("Bingo! Correct answer");
+// } else if (userGuess + 1 === secretNumber) {
+//     alert("Close enough to the correct answer");
+// } else {
+//     alert("Wrong guess! The secret number was " + secretNumber);
+// }
+
+// Question no 8
+
+// let num = +prompt("Enter your number");
+
+// if (num % 3 === 0) {
+//     document.write("<h1>The number is divisible by 3</h1>")
+// }else{
+//     document.write("<h1>The number is not divisible by 3</h1>")
+// }
+
+// Question no 9
+
+// let num = +prompt("Enter your number");
+
+// if (num % 2 === 0) {
+//     document.write("<h1>You enter even number</h1>")
+// }else{
+//     document.write("<h1>Your num is odd number</h1>")
+// }
+
+// Question no 10
+
+// let temperature = +prompt("Enter temperature");
+
+// if (temperature >= 40) {
+//     document.write("<h1>It is too hot outside.</h1>")
+// }else if (temperature >= 30) {
+//     document.write("<h1>The Weather today is Normal.</h1>")
+// }else if (temperature >= 20) {
+//     document.write("<h1>Today’s weather is so Cool.</h1>")
+// }else if (temperature >= 10) {
+//     document.write("<h1>OMG! Today’s weather is so Cool.</h1>")
+// }
+
+// Question no 11
+
+// let num1 = +prompt("Please enter first number");
+// let opreator = prompt("Please enter opreator +,-,*,/ & %");
+// let num2 = +prompt("Please enter second number");
+
+// if (opreator === "+") {
+//     let sum = num1 + num2;
+//     document.write(`<h1>${num1} ${opreator} ${num2} = ${sum}</h1>`)
+// };
+// if (opreator === "-") {
+//     let sub = num1 - num2;
+//     document.write(`<h1>${num1} ${opreator} ${num2} = ${sub}</h1>`)
+// };
+// if (opreator === "*") {
+//     let multiply = num1 * num2;
+//     document.write(`<h1>${num1} ${opreator} ${num2} = ${multiply}</h1>`)
+// };
+// if (opreator === "/") {
+//     let divide = num1 / num2;
+//     document.write(`<h1>${num1} ${opreator} ${num2} = ${divide}</h1>`)
+// };
+// if (opreator === "%") {
+//     let remainder = num1 % num2;
+//     document.write(`<h1>${num1} ${opreator} ${num2} = ${remainder}</h1>`)
+// };
+
+// Chapter no 9 to 11 is completed
+
+// Chapter no 12 to 13
+
+// Question no 1
+
+// let input = prompt("Enter any 1 character"); // Change this to test different inputs
+
+// let asciiCode = input.charCodeAt(0);
+
+// if (asciiCode >= 48 && asciiCode <= 57) {
+//     document.write("The input is a number.");
+// }else if (asciiCode >= 65 && asciiCode <= 90) {
+//     document.write("The input is an uppercase letter.");
+// }else if (asciiCode >= 97 && asciiCode <= 122) {
+//     document.write("The input is a lowercase letter.");
+// }else {
+//     document.write("The input is neither a number nor a letter.");
+// }
+
+// Question no 2
+
+// let num1 = Number(prompt("Enter the first integer:"));
+// let num2 = Number(prompt("Enter the second integer:"));
+
+// if (num1 > num2) {
+//     document.write("The larger integer is: " + num1);
+// } else if (num2 > num1) {
+//     document.write("The larger integer is: " + num2);
+// } else {
+//     document.write("Both integers are equal.");
+// }
+
+// Question no 3
+
+// let num = Number(prompt("Enter a number"));
+
+// if (num > 0) {
+//     document.write("<h1>The number is positive</h1>")
+// }else if (num < 0) {
+//     document.write("<h1>The number is negative</h1>")
+// }else {
+//     document.write("<h1>The number is zero</h1>")
+// }
+
+// Question no 4
+
+// let character = prompt("Enter any 1 alphabet character");
+// let vowel = "aeiouAEIOU"
+
+// if (vowel.includes(character)) {
+//     document.write("<h1>true</h1>")
+// }else {
+//     document.write("<h1>false</h1>")    
+// }
+
+// Question no 5
+
+// let password = "password";
+// let userPassword = prompt("Enter your password");
+//  if (userPassword === "") {
+//     document.write("<h1>Please enter your password</h1>")
+// }else if (password === userPassword) {
+//     document.write("<h1>Correct! The password you entered matches the original password.</h1>")
+// }else if (password !== userPassword) {
+//     document.write("<h1>Incorrect password.</h1>")
+// }
+
+// Question no 6
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }else {
+// greeting = "Good evening";
+// }
+
+// Question no 7
+
+// let time = prompt("Enter time in 24 hours format: ");
+
+// if (time >= 0 && time <= 12) {
+//     document.write("<h1>Good morning!</h1>");
+// }else if (time >= 12 && time <= 17) {
+//     document.write("<h1>Good afternoon!</h1>");
+// }else if (time >= 17 && time <= 21) {
+//     document.write("<h1>Good evening!</h1>");
+// }else if (time >= 21 && time <= 24) {
+//     document.write("<h1>Good night!</h1>");
+// }
+
+// Chapter no 12 to 13 completed
